965e0863136dee8005458e3f334659f4af2d1def
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-14 09:02:57 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  puts "sum"
+  num1 + num2
 end
-
+addition(1, 9)
 def subtraction(num1, num2)
-
+  puts "subtract two numbers from each other"
+  num1 - num2
 end
-
+subtraction(10, 3)
 def division(num1, num2)
-
+  puts "divide one number from another number"
+  num1/num2
 end
-
+division(100, 5)
 def multiplication(num1, num2)
-
+  puts "take two numbers and multibly each other"
+  num1 * num2
 end
-
+multiplication(12, 2)
 def modulo(num1, num2)
-
+  puts "returns the reminder of the number"
+  num1 % num2
 end
-
+modulo(34, 5)
 def square_root(num)
-
+  puts "returns the square root of the number"
 end

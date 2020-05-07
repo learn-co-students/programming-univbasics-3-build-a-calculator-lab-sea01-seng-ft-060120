0f53691fdd81633ca37a3064b5677ce37690beac
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-07 15:53:02 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  math = num1+num2
+  math
 end
 
 def subtraction(num1, num2)
-
+  math = num1-num2
+  math
 end
 
 def division(num1, num2)
-
+  math = num1/num2
+  math
 end
 
 def multiplication(num1, num2)
-
+  math = num1*num2
+  math
 end
 
 def modulo(num1, num2)
-
+  math = num1%num2
+  math
 end
 
 def square_root(num)
-
+  math = Math.sqrt(num)
+  math
 end
